37967eb98ed4d5b6dcfd58ea153c75e18c855d7c
Anna Goetz <anna.gtz087@outlook.com>
2023-06-30 21:38:53 +0200

    changed CV

--- script.js
+++ script.js
@@ -36,14 +36,14 @@ $(document).ready(function(){
   // typing text animation script
   var typed = new Typed(".typing", {
       //strings: ["YouTuber", "Developer", "Blogger", "Designer", "Freelancer"],//
-      strings: ["Junior Pricing Analyst"],
+      strings: ["Versatile Professional"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
   });
 
   var typed = new Typed(".typing-2", {
-      strings: ["Junior Pricing Analyst"],
+      strings: ["Versatile Professional"],
       typeSpeed: 100,
       backSpeed: 60,
       loop: true
